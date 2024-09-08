4858410c1e8e579a87686c74685d72a6937a66dd
unknown <johncarloaltura705@gmail.com>
2024-09-08 15:04:54 +0800

    Completed Variables Section

---
+++ app.js
@@ -0,0 +1,30 @@
+//Using Variable
+console.log("Hello, World!");
+let name = "John Carlo Altura";
+console.log('My name is ' + name);
+
+//Using Numbers
+let num1 = 19;
+let num2 = 27;
+let sum = num1 + num2;
+console;
+
+//Using Booleans 
+let isActive = true;
+let inSubcribed = false;
+console.log(isActive);
+console.log(inSubcribed);
+
+//Using Comparison-Operator
+let A = 8;
+let B = 18;
+
+let isAGreaterThanB = A > B;
+let isALessThanB = A < B;
+let isAEqualToB = A == B;
+let isANotEqualToB = A !== B;
+
+console.log('Is A greater than B?', isAGreaterThanB);
+console.log('Is A less than B?', isALessThanB);
+console.log('is A equealto B?', isAEqualToB);
+console.log('is not equal B?', isANotEqualToB);
